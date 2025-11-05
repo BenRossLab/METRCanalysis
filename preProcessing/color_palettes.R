e74cb02df8981304d6927c94a48f9e6a125117fb
BenRossLab <134626505+BenRossLab@users.noreply.github.com>
2025-11-05 12:08:45 -0500

    Create color_palettes.R

---
+++ preProcessing/color_palettes.R
@@ -0,0 +1,79 @@
+########## Define color palettes ##########
+# 36 distinct colors
+# Greens, Reds, Blues, Oranges, Purples, Yellows, Cyans, Pinks, Grays
+pal.pairBiome<-c("#1c541c","#2f8f2f","#49c349","#bfeabf",
+                 "#B80000","#F00000","#FF7777","#ffcccc",
+                 "#000080","#0000cd","#8282ff","#cfcfff",
+                 "#623800","#c47000","#ff9914","#ffddb1",
+                 "#430059","#7d00a7","#cc32ff","#eebbff",
+                 "#626200","#c4c400","#ffff14","#ffffb1",
+                 "#005f6c","#00a4bb","#1ee3ff","#bbf7ff",
+                 "#750063","#c400a5","#ff13da","#ffb0f3",
+                 "#1a1a1a","#808080","#d9d9d9","#ffffff"
+                 )
+
+# 19 distinct colors
+# Greens, Reds, Blues, Oranges, Purples, Yellows, Cyans, Pinks, Grays
+pal.pairMini<-c("#B2DF8A","#33A02C",
+                "#FB9A99","#E31A1C",
+                "#A6CEE3","#1F78B4",
+                "#FDBF6F","#FF7F00",
+                "#CAB2D6","#6A3D9A",
+                "#FFFF99","#eded09",
+                "#9aebff","#01cdff",
+                "#ffb0f3","#E511C4",
+                "#e6e6e6","#999999","#404040"
+                )
+
+# COLOR-BLIND FRIENDLY
+# 10 distinct colors
+pal.CB<-c("#e69f00","#56b4e9","#009e73",
+          "#f0e442","#0072b2","#D55e00",
+          "#cc79a7","#999999","#000000","#ffffff"
+          )
+
+# palette used for synthetic gut microbiome by Desai et al, 2016
+# 15 colors
+pal.SGM<-c("#5087E3","#8F0238","#F049CB","#FFFA00","#008136","#A7A7A7","#000000","#F2A122",
+           "#7548B4","#55C41F","#5087E3","#CFC6F1","#e90909","#A16910","#00CFFF"
+) # palette used for synthetic gut microbiome by Desai et al, 2016
+
+
+pal.heatBlue <- c("#C1C1C1",
+                  "#ecf3ff",
+                  "#d9e7ff",
+                  "#c6dbff",
+                  "#b3cfff",
+                  "#a0c4ff",
+                  "#8db8ff",
+                  "#7aacff",
+                  "#67a0ff",
+                  "#5494ff",
+                  "#4188ff",
+                  "#3b7ae6",
+                  "#346dcc",
+                  "#2e5fb3",
+                  "#275299",
+                  "#214480",
+                  "#1a3666")
+
+pal.heatRed <- c("#C1C1C1",
+                 "#f2e5e5",
+                 "#e5cccc",
+                 "#d8b2b2",
+                 "#cc9999",
+                 "#bf7f7f",
+                 "#b26666",
+                 "#a64c4c",
+                 "#993232",
+                 "#8c1919",
+                 "#800000",
+                 "#730000",
+                 "#660000",
+                 "#590000",
+                 "#4c0000",
+                 "#400000",
+                 "#330000")
+
+
+
